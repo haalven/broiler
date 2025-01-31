9c9ef2b7a291a543ad5fc2cfad2d4770c8c6ec7c
haalven <147269352+haalven@users.noreply.github.com>
2025-01-31 22:11:29 +0100

    Create broiler.py

---
+++ broiler.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# or #!/usr/bin/python3
+# or #!/path/to/.venv/bin/python3
+
+# name of the program
+# what is this?
+# requirements?
+# syntax: program [--option] parameter
+
+import sys
+import os.path
+import tomllib
+import argparse
+
+# read TOML file
+def read_configuration(my_dir, my_name):
+    config_path = os.path.join(my_dir, my_name + '.toml')
+    try:
+        with open(config_path, 'rb') as f:
+            return tomllib.load(f)
+    except: return None
+
+# parse arguments
+def get_arguments(my_name):
+    parser = argparse.ArgumentParser(prog=my_name)
+    parser.add_argument('parameter',
+                        type=str,
+                        help='parameter help')
+    parser.add_argument('--option',
+                        action='store_true',
+                        required=False,
+                        help='option help')
+    return parser.parse_args()
+
+
+def main() -> int:
+
+    # my path
+    my_path = os.path.abspath(__file__)
+    my_dir = os.path.dirname(my_path)
+    my_name = os.path.basename(my_path)
+
+    # load configuration file
+    config = read_configuration(my_dir, my_name)
+
+    # get arguments.parameter and arguments.option
+    arguments = get_arguments(my_name)
+
+    # fancy code starts here
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
